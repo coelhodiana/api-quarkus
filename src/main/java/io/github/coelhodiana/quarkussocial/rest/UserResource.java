8aae3d20218065e32bd25fbedf5dae318967ae44
Diana Coelho <coelhodiana04@gmail.com>
2024-10-13 13:39:12 -0300

    feat: padroniza as respostas de erro

--- src/main/java/io/github/coelhodiana/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/coelhodiana/quarkussocial/rest/UserResource.java
@@ -1,33 +1,59 @@
 package io.github.coelhodiana.quarkussocial.rest;
 
+import io.github.coelhodiana.domain.repository.UserRepository;
 import io.github.coelhodiana.quarkussocial.domain.model.User;
 import io.github.coelhodiana.quarkussocial.rest.dto.CreateUserRequest;
+import io.github.coelhodiana.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.Set;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
+
+    private final UserRepository repository;
+    private final Validator validator;
+
+    @Inject
+    public UserResource(UserRepository repository, Validator validator) {
+        this.repository = repository;
+        this.validator = validator;
+    }
+
+
     @POST
     @Transactional
     public Response createUser( CreateUserRequest userRequest ) {
+
+        Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
+
+        if(!violations.isEmpty()) {
+            ResponseError responseError = ResponseError.createFromValidation(violations);
+            return Response.status(400).entity(responseError).build();
+        }
+
         User user = new User();
         user.setName(userRequest.getName());
         user.setAge(userRequest.getAge());
 
-        user.persist();
+        repository.persist(user);
 
         return Response.ok(user).build();
     }
 
     @GET
     public Response listAllUsers() {
-        PanacheQuery<PanacheEntityBase> query = User.findAll();
+        PanacheQuery<User> query = repository.findAll();
         return Response.ok(query.list()).build();
     }
 
@@ -35,10 +61,10 @@ public class UserResource {
     @Path("{id}")
     @Transactional
     public Response deleteUser (@PathParam("id") Long id) {
-        User user = User.findById(id);
+        User user = repository.findById(id);
 
         if(user != null){
-            user.delete();
+            repository.delete(user);
             return Response.ok().build();
         }
 
@@ -50,7 +76,7 @@ public class UserResource {
     @Path("{id}")
     @Transactional
     public Response updateUser (@PathParam("id") Long id, CreateUserRequest userData) {
-        User user = User.findById(id);
+        User user = repository.findById(id);
 
         if(user != null) {
             user.setName(userData.getName());
--- src/main/java/io/github/coelhodiana/quarkussocial/rest/dto/CreateUserRequest.java
+++ src/main/java/io/github/coelhodiana/quarkussocial/rest/dto/CreateUserRequest.java
@@ -1,7 +1,13 @@
 package io.github.coelhodiana.quarkussocial.rest.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class CreateUserRequest {
+
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotNull(message = "Age is required")
     private Integer age;
 
     public String getName() {
